766fff1a2b76ea19f410bbfc99f06e29b5fc357d
MariaFernandaMiranda <mfmir1974@gmail.com>
2021-11-30 21:44:25 -0300

    modified

--- WhisKING/src/controllers/productsController.js
+++ WhisKING/src/controllers/productsController.js
@@ -1,15 +1,13 @@
 let controller = {
     detail: (req, res) => {
-<<<<<<< HEAD
         res.render('products/productDetail', { title: 'Detalle de productos', img: 'src="../images/logo.svg"', img: 'src="../images/logo2.svg"'});
 
     },
     create:(req, res) =>{
         res.render('products/productCreate')
->>>>>>> 0711eb64b04a3a9347b91536639262f99a250fde
+
     }
 }
 
 module.exports = controller;
 
-img: 'src="../images/logo.svg"'
