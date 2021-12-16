c4b8dafae727d7a44e274619a34fceece7e30dcd
MariaFernandaMiranda <mfmir1974@gmail.com>
2021-12-15 23:54:41 -0300

    modify partials, productCreate, productsController

--- WhisKING/src/controllers/productsController.js
+++ WhisKING/src/controllers/productsController.js
@@ -9,11 +9,11 @@ const writeJson = dataBase => {
 
 let controller = {
     detail: (req, res) => {
-        res.render('products/productDetail', { title: 'Detalle de producto', img: 'src="../images/logo.svg"', img2:'src="../images/logo2.svg"'});
+        res.render('products/productDetail');
 
     },
     create:(req, res) =>{
-        res.render('products/productCreate', { title: 'Crear producto', img3: 'src="../images/logoCobre.svg"', img4:'src="../images/isologoCobre.svg"'});
+        res.render('products/productCreate');
 
     },
    
