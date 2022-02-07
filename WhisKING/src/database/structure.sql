7b867a446b1d9b7a807d2d17a7803abb7de00310
francobanfield01 <franco_banfield@hotmail.com>
2022-02-07 13:08:15 -0300

    Structure script

--- WhisKING/src/database/structure.sql
+++ WhisKING/src/database/structure.sql
@@ -1,170 +1,299 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: the_whisking
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.22-MariaDB
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema whisking
--- -----------------------------------------------------
+--
+-- Table structure for table `categories`
+--
 
--- -----------------------------------------------------
--- Schema whisking
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `whisking` DEFAULT CHARACTER SET utf8 ;
-USE `whisking` ;
-
--- -----------------------------------------------------
--- Table `whisking`.`personal_information`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `whisking`.`personal_information` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `address` VARCHAR(100) NULL,
-  `province` VARCHAR(30) NULL,
-  `city` VARCHAR(30) NULL,
-  `postal_code` VARCHAR(15) NULL,
-  `createdAt` TIMESTAMP(15) NULL,
-  `updatedAt` TIMESTAMP(15) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `whisking`.`order_carts`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `whisking`.`order_carts` (
-  `id` INT(11) NOT NULL,
-  `user_id` INT(11) NOT NULL,
-  `state` VARCHAR(45) NOT NULL,
-  `createdAt` TIMESTAMP(15) NULL,
-  `updatedAt` TIMESTAMP(15) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `whisking`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `whisking`.`users` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NOT NULL,
-  `last_name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(70) NOT NULL,
-  `pass` VARCHAR(70) NOT NULL,
-  `rol` TINYINT(2) NOT NULL,
-  `date_of_birth` DATE NOT NULL,
-  `phone` VARCHAR(30) NULL,
-  `cell_phone` VARCHAR(30) NULL,
-  `image` VARCHAR(100) NULL,
-  `order_cart_id` INT(11) NULL,
-  `personal_information_id` INT(11) NOT NULL,
-  `createdAt` TIMESTAMP(15) NULL,
-  `updatedAt` TIMESTAMP(15) NULL,
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `banner` varchar(60) NOT NULL,
+  `product_id` int(11) unsigned NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-  CONSTRAINT `personal_information_id`
-    FOREIGN KEY ()
-    REFERENCES `whisking`.`personal_information` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `order_cart_id`
-    FOREIGN KEY ()
-    REFERENCES `whisking`.`order_carts` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `categories_FK` (`product_id`),
+  CONSTRAINT `categories_FK` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `categories`
+--
 
--- -----------------------------------------------------
--- Table `whisking`.`order-items`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `whisking`.`order-items` (
-  `id` INT(11) NOT NULL,
-  `order_cart_id` INT(11) NOT NULL,
-  `product_id` INT(11) NOT NULL,
-  `quantity` INT(11) NOT NULL,
-  `date_time` DATETIME(20) NULL,
-  `createdAt` TIMESTAMP(15) NOT NULL,
-  `updatedAt` TIMESTAMP(15) NOT NULL,
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `images` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order-item`
+--
+
+DROP TABLE IF EXISTS `order-item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order-item` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `quantity` int(11) unsigned NOT NULL,
+  `date_time` datetime NOT NULL,
+  `order_cart_id` int(11) unsigned NOT NULL,
+  `product_id` int(11) unsigned NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  CONSTRAINT `order_cart_id`
-    FOREIGN KEY ()
-    REFERENCES `whisking`.`order_carts` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `order_item_FK` (`order_cart_id`),
+  KEY `order_item_FK_1` (`product_id`),
+  CONSTRAINT `order_item_FK` FOREIGN KEY (`order_cart_id`) REFERENCES `order_carts` (`id`),
+  CONSTRAINT `order_item_FK_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `order-item`
+--
 
--- -----------------------------------------------------
--- Table `whisking`.`categories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `whisking`.`categories` (
-  `id` INT(11) NOT NULL,
-  `name` VARCHAR(45) NULL,
-  `banner` VARCHAR(60) NULL,
-  `createdAt` TIMESTAMP(15) NULL,
-  `updatedAt` TIMESTAMP(15) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+LOCK TABLES `order-item` WRITE;
+/*!40000 ALTER TABLE `order-item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order-item` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `order_carts`
+--
 
--- -----------------------------------------------------
--- Table `whisking`.`products`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `whisking`.`products` (
-  `id` INT(11) NOT NULL,
-  `name` VARCHAR(200) NOT NULL,
-  `price` DECIMAL(11) UNSIGNED NOT NULL,
-  `discount` TINYINT(3) UNSIGNED NOT NULL,
-  `description` TEXT(800) NULL,
-  `cata` TEXT(800) NULL,
-  `origen` CHAR(20) NULL,
-  `stock` INT(10) NULL,
-  `productscol` VARCHAR(45) NULL,
-  `image_id` INT(11) NULL,
-  `category_id` INT(11) NULL,
-  `createdAt` TIMESTAMP(15) NOT NULL,
-  `updatedAt` TIMESTAMP(15) NOT NULL,
+DROP TABLE IF EXISTS `order_carts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_carts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `state` varchar(45) NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  CONSTRAINT `order_item_id`
-    FOREIGN KEY ()
-    REFERENCES `whisking`.`order-items` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `category_id`
-    FOREIGN KEY ()
-    REFERENCES `whisking`.`categories` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `order_carts_FK` (`user_id`),
+  CONSTRAINT `order_carts_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `order_carts`
+--
 
--- -----------------------------------------------------
--- Table `whisking`.`timestamps`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `whisking`.`timestamps` (
-  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` TIMESTAMP NULL);
+LOCK TABLES `order_carts` WRITE;
+/*!40000 ALTER TABLE `order_carts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_carts` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `personal_informations`
+--
 
--- -----------------------------------------------------
--- Table `whisking`.`images`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `whisking`.`images` (
-  `id` INT(11) NOT NULL,
-  `image` VARCHAR(100) NOT NULL,
-  `product_id` INT(11) NOT NULL,
-  `createdAt` TIMESTAMP(15) NULL,
-  `updatedAt` TIMESTAMP(15) NULL,
+DROP TABLE IF EXISTS `personal_informations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personal_informations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `address` varchar(100) NOT NULL,
+  `province` varchar(30) NOT NULL,
+  `city` varchar(30) NOT NULL,
+  `postal_code` varchar(15) NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  CONSTRAINT `product_id`
-    FOREIGN KEY ()
-    REFERENCES `whisking`.`products` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `personal_informations_FK` (`user_id`),
+  CONSTRAINT `personal_informations_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `personal_informations`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `personal_informations` WRITE;
+/*!40000 ALTER TABLE `personal_informations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `personal_informations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `price` decimal(3,2) unsigned NOT NULL,
+  `discount` decimal(3,2) unsigned NOT NULL,
+  `description` text DEFAULT NULL,
+  `cata` text DEFAULT NULL,
+  `origen` char(20) DEFAULT NULL,
+  `stock` int(10) unsigned NOT NULL,
+  `image_id` int(11) unsigned NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `products_FK` (`image_id`),
+  CONSTRAINT `products_FK` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `roles_FK` (`user_id`),
+  CONSTRAINT `roles_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stocks`
+--
+
+DROP TABLE IF EXISTS `stocks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stocks` (
+  `id` int(11) unsigned NOT NULL,
+  `quantity` int(11) unsigned NOT NULL,
+  `product_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `stocks_FK` (`product_id`),
+  CONSTRAINT `stocks_FK` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stocks`
+--
+
+LOCK TABLES `stocks` WRITE;
+/*!40000 ALTER TABLE `stocks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stocks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `pass` varchar(70) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `cell_phone` varchar(30) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `phone` (`phone`),
+  UNIQUE KEY `cell_phone` (`cell_phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'the_whisking'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-07 12:34:23
